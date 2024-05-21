0d79ee5bbb4724f6e775e274817b9f7d61b6937c
jmiguelcn <113700439+jmiguelcn@users.noreply.github.com>
2024-05-21 16:03:41 +0200

    Upload project

---
+++ PPB_Installer/Controller/InstallerController.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace PPB_Installer.Controller
+{
+    public class InstallerController
+    {
+        InstallerForm f = new InstallerForm();
+
+        public InstallerController()
+        {
+            LoadData();
+            InitListeners();
+            Application.Run(f);
+        }
+
+        void LoadData()
+        {
+
+        }
+
+        void InitListeners()
+        {
+            f.install.Click += Install;
+        }
+
+        private void Install(object sender, EventArgs e)
+        {
+            f.install.Enabled = false;
+
+            CreateDatabase();
+            CreateTables();
+            CreateRegisters();
+
+            f.messageText.Text = "La instalació s'ha completat exitosament.";
+
+            f.install.Text = "Finalitzar";
+            f.install.Click -= Install;
+            f.install.Click += End;
+            f.install.Enabled = true;
+        }
+
+        private void End(object sender, EventArgs e)
+        {
+            f.Close();
+        }
+
+        private void CreateDatabase()
+        {
+            string conStr = "Server=.\\sqlexpress;Integrated security=SSPI;database=master";
+
+            SqlConnection con = new SqlConnection(conStr);
+
+            if (con.State != System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                    con.Open();
+
+                    f.messageText.Text = "Eliminant base de dades anterior...";
+                    SqlCommand command = new SqlCommand("DROP DATABASE IF EXISTS [PPB_Storage]", con);
+
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+
+                    f.messageText.Text = "Creant base de dades...";
+                    command = new SqlCommand("CREATE DATABASE [PPB_Storage]", con);
+
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        private void CreateTables()
+        {
+            string conStr = "Server=.\\sqlexpress;Integrated security=SSPI;database=PPB_Storage";
+
+            SqlConnection con = new SqlConnection(conStr);
+
+            if (con.State != System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                    con.Open();
+
+                    f.messageText.Text = "Creant taules...";
+                    SqlCommand command = new SqlCommand("CREATE TABLE \"products\" (" +
+                        "\"id\" int IDENTITY (1, 1) NOT NULL PRIMARY KEY," +
+                        "\"barcode\" nvarchar(13) NOT NULL UNIQUE," +
+                        "\"image\" nvarchar(255) NOT NULL," +
+                        "\"name\" nvarchar(255) NOT NULL," +
+                        "\"price\" real NOT NULL," +
+                        ")", con);
+
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+
+                    command = new SqlCommand("CREATE TABLE \"commands\" (" +
+                        "\"id\" int IDENTITY (1, 1) NOT NULL PRIMARY KEY," +
+                        "\"number\" int NOT NULL," +
+                        "\"date\" date NOT NULL," +
+                        "\"ready\" bit NOT NULL," +
+                        "\"delivered\" bit NOT NULL," +
+                        ")", con);
+
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+
+                    command = new SqlCommand("CREATE TABLE \"products_commands\"(" +
+                        "\"id\" int IDENTITY(1, 1) NOT NULL PRIMARY KEY," +
+                        "\"commandId\" int NOT NULL FOREIGN KEY REFERENCES \"dbo\".\"commands\"(\"id\")," +
+                        "\"productId\" int NOT NULL FOREIGN KEY REFERENCES \"dbo\".\"products\"(\"id\")," +
+                        "\"quantity\" int NOT NULL," +
+                        ")", con);
+
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+
+                    command = new SqlCommand("CREATE TABLE \"roles\" (" +
+                        "\"id\" int IDENTITY (1, 1) NOT NULL PRIMARY KEY," +
+                        "\"name\" nvarchar(255) NOT NULL," +
+                        ")", con);
+
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+
+                    command = new SqlCommand("CREATE TABLE \"users\" (" +
+                        "\"id\" int IDENTITY (1, 1) NOT NULL PRIMARY KEY," +
+                        "\"first_name\" nvarchar(255) NOT NULL," +
+                        "\"last_name\" nvarchar(255) NOT NULL," +
+                        "\"username\" nvarchar(255) NOT NULL UNIQUE," +
+                        "\"password\" nvarchar(255) NOT NULL," +
+                        "\"role_id\" int NOT NULL FOREIGN KEY REFERENCES \"dbo\".\"roles\"(\"id\")," +
+                        ")", con);
+
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+
+                    command = new SqlCommand("CREATE TABLE \"permissions\" (" +
+                        "\"id\" int IDENTITY (1, 1) NOT NULL PRIMARY KEY," +
+                        "\"name\" nvarchar(255) NOT NULL," +
+                        ")", con);
+
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+
+                    command = new SqlCommand("CREATE TABLE \"permissions_roles\" (" +
+                        "\"id\" int IDENTITY (1, 1) NOT NULL PRIMARY KEY," +
+                        "\"roleId\" int NOT NULL FOREIGN KEY REFERENCES \"dbo\".\"roles\"(\"id\")," +
+                        "\"permissionId\" int NOT NULL FOREIGN KEY REFERENCES \"dbo\".\"permissions\"(\"id\")," +
+                        ")", con);
+
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        private void CreateRegisters()
+        {
+            string conStr = "Server=.\\sqlexpress;Integrated security=SSPI;database=PPB_Storage";
+
+            SqlConnection con = new SqlConnection(conStr);
+
+            if (con.State != System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                    con.Open();
+
+                    f.messageText.Text = "Creant registres...";
+                    SqlCommand command = new SqlCommand("SET IDENTITY_INSERT [dbo].[permissions] ON", con);
+                    command.ExecuteNonQuery();
+
+                    command = new SqlCommand("INSERT INTO[dbo].[permissions] ([id], [name]) VALUES " +
+                        "(1, 'READ USERS')," +
+                        "(2, 'WRITE USERS')," +
+                        "(3, 'READ ROLES')," +
+                        "(4, 'WRITE ROLES')," +
+                        "(5, 'READ PRODUCTS')," +
+                        "(6, 'WRITE PRODUCTS')," +
+                        "(7, 'READ COMMANDS')," +
+                        "(8, 'WRITE COMMAND')", con);
+                    command.ExecuteNonQuery();
+
+                    command = new SqlCommand("SET IDENTITY_INSERT [dbo].[permissions] OFF", con);
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+
+                    command = new SqlCommand("SET IDENTITY_INSERT [dbo].[roles] ON", con);
+                    command.ExecuteNonQuery();
+
+                    command = new SqlCommand("INSERT INTO [dbo].[roles] ([id], [name]) VALUES " +
+                        "(1, 'Administrator')", con);
+                    command.ExecuteNonQuery();
+
+                    command = new SqlCommand("SET IDENTITY_INSERT [dbo].[roles] OFF", con);
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+
+                    command = new SqlCommand("SET IDENTITY_INSERT [dbo].[users] ON", con);
+                    command.ExecuteNonQuery();
+
+                    command = new SqlCommand("INSERT INTO [dbo].[users] ([id], [first_name], [last_name], [username], [password], [role_id]) VALUES " +
+                        "(1, 'John', 'Doe', 'admin', '$2y$10$7U/oWytzZkWSnB7CRbTSM.6hETZcEuqWNDRvhS1LDbBnOJALpE7Ee', 1)", con);
+                    command.ExecuteNonQuery();
+
+                    command = new SqlCommand("SET IDENTITY_INSERT [dbo].[users] OFF", con);
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+
+                    command = new SqlCommand("INSERT INTO [dbo].[permissions_roles] ([roleId], [permissionId]) VALUES " +
+                        "(1, 1)," +
+                        "(1, 2)," +
+                        "(1, 3)," +
+                        "(1, 4)," +
+                        "(1, 5)," +
+                        "(1, 6)," +
+                        "(1, 7)," +
+                        "(1, 8)", con);
+                    command.ExecuteNonQuery();
+
+                    f.loadingBar.Value++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
+}
---
+++ PPB_Installer/View/InstallerForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PPB_Installer
+{
+    partial class InstallerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstallerForm));
+            this.loadingBar = new System.Windows.Forms.ProgressBar();
+            this.install = new System.Windows.Forms.Button();
+            this.messageText = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loadingBar
+            // 
+            this.loadingBar.Location = new System.Drawing.Point(12, 415);
+            this.loadingBar.Maximum = 13;
+            this.loadingBar.Name = "loadingBar";
+            this.loadingBar.Size = new System.Drawing.Size(539, 23);
+            this.loadingBar.TabIndex = 0;
+            // 
+            // install
+            // 
+            this.install.Location = new System.Drawing.Point(557, 415);
+            this.install.Name = "install";
+            this.install.Size = new System.Drawing.Size(140, 23);
+            this.install.TabIndex = 1;
+            this.install.Text = "Instalar";
+            this.install.UseVisualStyleBackColor = true;
+            // 
+            // messageText
+            // 
+            this.messageText.Location = new System.Drawing.Point(12, 386);
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(370, 23);
+            this.messageText.TabIndex = 2;
+            this.messageText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::PPB_Installer.Properties.Resources.logo_nb;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 220);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(81)))), ((int)(((byte)(245)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(388, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(309, 397);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 88);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Benvingut al instalador de la base de dades del projecte Phone Basket";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(370, 264);
+            this.label2.TabIndex = 6;
+            this.label2.Text = resources.GetString("label2.Text");
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // InstallerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.install);
+            this.Controls.Add(this.loadingBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "InstallerForm";
+            this.Text = "Instalador de la base de dades del projecte Phone Basket";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ProgressBar loadingBar;
+        public System.Windows.Forms.Button install;
+        public System.Windows.Forms.Label messageText;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
